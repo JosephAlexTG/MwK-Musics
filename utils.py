49f0a07defde69bf4b15a2dd738f85f020e9c1ec
JosephAlexTG <98728675+JosephAlexTG@users.noreply.github.com>
2022-02-16 10:39:03 +0530

    🤐

--- utils.py
+++ utils.py
@@ -1,22 +1,3 @@
-# A Subinps Project
-# Pyrogram - Telegram MTProto API Client Library for Python
-# Copyright (C) 2017-2020 Dan <https://github.com/delivrance>
-#
-# This file is part of Pyrogram.
-#
-# Pyrogram is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Pyrogram is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
 
 
 import os
@@ -43,7 +24,7 @@ from user import USER
 STREAM_URL=Config.STREAM_URL
 CHAT=Config.CHAT
 GROUP_CALLS = {}
-FFMPEG_PROCESSES = {}
+FFMPEG_PROCESSES =
 RADIO={6}
 LOG_GROUP=Config.LOG_GROUP
 DURATION_LIMIT=Config.DURATION_LIMIT
